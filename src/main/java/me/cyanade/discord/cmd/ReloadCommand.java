ffef93270b318176ccd1ecae0cbde2a98132e743
Cyanade <88956343+Cyanade@users.noreply.github.com>
2022-08-19 03:31:27 +0100

    Add files via upload

---
+++ src/main/java/me/cyanade/discord/cmd/ReloadCommand.java
@@ -0,0 +1,24 @@
+package me.cyanade.discord.cmd;
+
+import me.cyanade.discord.Discord;
+import me.cyanade.discord.util.ColorUtil;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ReloadCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (commandSender.hasPermission("discord.reload") || commandSender.isOp()) {
+            commandSender.sendMessage(ColorUtil.translate("&bReloading Discord &7config.yml&b!"));
+            Discord.getInstance().saveDefaultConfig();
+            Discord.getInstance().reloadConfig();
+            Discord.getInstance().loadConfig();
+            commandSender.sendMessage(ColorUtil.translate("&bDone!"));
+            return true;
+        } else {
+            commandSender.sendMessage(ColorUtil.translate(Discord.getInstance().getPermission()));
+            return true;
+        }
+    }
+}
